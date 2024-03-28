6a991af10f280634af0859ba59c4196c2753bb6b
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2024-03-28 22:41:26 +0200

    fixed build error

--- components/ChatBox/ChatBox.tsx
+++ components/ChatBox/ChatBox.tsx
@@ -12,38 +12,43 @@ type ChatBoxType = {
   ref: React.MutableRefObject<null>;
 };
 
-const ChatBox = forwardRef<HTMLDivElement, ChatBoxType>(({ messages, user }, ref) => {
-  return (
-    <div className=" overflow-auto pr-10">
-      <div className="flex flex-col  gap-3 mt-3 pb-8">
-        {messages
-          ?.sort((a, b) => {
-            return (a.time as number) - (b.time as number);
-          })
-          .map((chat) => {
-            return (
-              <div
-                key={chat.id}
-                className={classNames(
-                  chat.email === user?.email ? "justify-start" : "justify-end",
-                  "flex min-h-12 items-end gap-2"
-                )}
-              >
-                <div>
-                  <Avatar name={chat.email === user?.email ? chat.sender : chat.receiverName} size="40" round={true} />
-                </div>
-                <div className={"text-black min-h-12 rounded-lg px-3 py-1 bg-white"}>
-                  <div className="text-base font-bold">
-                    {chat.email === user?.email ? chat.sender : chat.receiverName}
+const ChatBox = forwardRef<HTMLDivElement, ChatBoxType>(
+  function ChatBoxComponent({ messages, user }, ref) {
+    return (
+      <div className=" overflow-auto pr-10">
+        <div className="flex flex-col  gap-3 mt-3 pb-8">
+          {messages
+            ?.sort((a, b) => {
+              return (a.time as number) - (b.time as number);
+            })
+            .map((chat) => {
+              return (
+                <div
+                  key={chat.id}
+                  className={classNames(
+                    chat.email === user?.email ? "justify-start" : "justify-end",
+                    "flex min-h-12 items-end gap-2"
+                  )}
+                >
+                  <div>
+                    <Avatar name={chat.email === user?.email ? chat.sender : chat.receiverName} size="40" round={true} />
                   </div>
-                  <div key={chat.id}>{chat.message}</div>
+                  <div className={"text-black min-h-12 rounded-lg px-3 py-1 bg-white"}>
+                    <div className="text-base font-bold">
+                      {chat.email === user?.email ? chat.sender : chat.receiverName}
+                    </div>
+                    <div key={chat.id}>{chat.message}</div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        <div ref={ref} />
+              );
+            })}
+          <div ref={ref} />
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
+
+ChatBox.displayName = "ChatBox";
+
 export default ChatBox;
